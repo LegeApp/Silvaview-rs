86839165158ca5039a447d9a7d9d1dde5952576f
dk <dk@local>
2026-03-01 10:36:52 -0800

    Remove dev artifacts, update ignore rules and README

--- run-elevated.ps1
+++
@@ -1,6 +0,0 @@
-$outputFile = "D:\Rust-projects\SilvaView-rs\scan-results.txt"
-$exe = "D:\Rust-projects\SilvaView-rs\target\release\debug-scan.exe"
-$args = "C:\"
-
-# Run elevated and redirect output to file
-Start-Process -Verb RunAs -FilePath $exe -ArgumentList $args -RedirectStandardOutput $outputFile -Wait -NoNewWindow
